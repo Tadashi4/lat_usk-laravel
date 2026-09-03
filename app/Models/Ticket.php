5400c74a918e6ff60fbbeb802fb2b7e21a48360c
Tadashi4 <tadashibahagiamartuasitumeang@gmail.com>
2026-09-03 11:30:08 +0700

    part 17 setengah

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -3,9 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\LogOptions;
+use Spatie\Activitylog\Traits\LogsActivity;
 
 class Ticket extends Model
 {
+
+use LogsActivity;
+
+public function getActivitylogOptions(): LogOptions
+{
+    return LogOptions::defaults()
+    ->logAll()
+    ->logOnlyDirty();
+}
     protected $fillable = [
 
     'user_id',
